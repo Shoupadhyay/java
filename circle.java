0a1bd8ecc93e0550f3ff8ae0d7cef3eef15f0409
Shoupadhyay <96110953+Shoupadhyay@users.noreply.github.com>
2022-09-02 22:03:15 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class circle {
+    public static void main(String[] args) {
+        Scanner radius = new Scanner(System.in);
+        System.out.println("Enter Radius:");
+        double n1=radius.nextDouble();
+        double area=(3.14*n1*n1);
+        double perimeter=2*3.14*n1;
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+
+    }
+    
+
---
+++ swap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class swap {
+    public static void main(String[] args) {
+        
+    
+Scanner sc=new Scanner(System.in);
+
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("the no in a and b are: " + a + " and " + b);
+
+int c=a;
+a=b;
+b=c;
+System.out.println(" the no in a and b now are: " + a + " and " + b);
+    }
+
+
+    
+}
